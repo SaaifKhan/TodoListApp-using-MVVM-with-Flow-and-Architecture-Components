7d360cc73ed399acb7c0330720ff86039d07acfc
saaifkhan <saif_khan09@hotmail.com>
2021-05-01 02:03:07 +0500

    update

---
+++ app/src/main/java/com/saif/mvvmtodo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.saif.mvvmtodo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.codinginflow.mvvmtodo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
